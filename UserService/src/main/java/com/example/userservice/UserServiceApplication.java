15dec535343fbd5d58f7b31243a88381455b8920
Yerkebulan <erkebulan434@gmail.com>
2024-09-07 18:12:04 +0500

    commit

---
+++ UserService/src/main/java/com/example/userservice/UserServiceApplication.java
@@ -0,0 +1,15 @@
+package com.example.userservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
+@SpringBootApplication
+@EnableJpaRepositories
+public class UserServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceApplication.class, args);
+    }
+
+}
---
+++ UserService/src/main/java/com/example/userservice/auth/RegisterRequest.java
@@ -0,0 +1,20 @@
+package com.example.userservice.auth;
+
+import com.example.userservice.Role;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+public class RegisterRequest {
+
+    private String firstname;
+    private String lastname;
+    private String email;
+    private String password;
+    private Role role;
+}
